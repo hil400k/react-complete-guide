21cdce1458eb7b8fc7be62ccb56d61a8af54286e
dmytro-khilchuk <Dmytro.Khilchuk@symphony-solutions.eu>
2019-03-12 11:06:26 +0200

    #manipulating state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'name1' },
+      { name: 'name2' },
+      { name: 'name3' }
+    ]
+  };
+  
+  switchNameHandler = () => {
+    console.info('clicked', this.state);
+    this.setState({
+      persons: [
+        { name: 'name1DUDULANT' },
+        { name: 'name2' },
+        { name: 'name3' }
+      ]
+    });
+  }
+  
   render() {
     return (
       <div className="App">
         <h1>Hi I am D</h1>
-          <Person name="name1"/>
-          <Person name="name2">Hobbies</Person>
-          <Person name="name3"/>
+          <button onClick={this.switchNameHandler}>Switch Me</button>
+          <Person name={this.state.persons[0].name}/>
+          <Person name={this.state.persons[1].name}>Hobbies</Person>
+          <Person name={this.state.persons[2].name}/>
       </div>
     );
     //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I am D'));
